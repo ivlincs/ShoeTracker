b8d27cbd3a3b33769746d609e9624be9a073f0b3
Ivaylo <ivlincs@gmail.com>
2026-03-31 19:18:21 +0300

    Add Comment entity linked to Shoe - users can leave notes on shoes

---
+++ ShoeTracker.Data.Models/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoeTracker.Data.Models.Entities
+{
+    internal class Comments
+    {
+    }
+}
