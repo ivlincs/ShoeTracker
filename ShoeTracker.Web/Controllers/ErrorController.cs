d33352144c4f8111195f06c11b71e5dee5ca1c88
Ivaylo <ivlincs@gmail.com>
2026-04-01 16:40:34 +0300

    Add ErrorController with custom 404 and 500 error pages

---
+++ ShoeTracker.Web/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoeTracker.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            return statusCode switch
+            {
+                404 => View("NotFound"),
+                500 => View("InternalServerError"),
+                _ => View("GenericError")
+            };
+        }
+    }
+}
--- ShoeTracker.Web/Program.cs
+++ ShoeTracker.Web/Program.cs
@@ -45,10 +45,12 @@ namespace ShoeTracker.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error/500");               
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithRedirects("/Error/{0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
