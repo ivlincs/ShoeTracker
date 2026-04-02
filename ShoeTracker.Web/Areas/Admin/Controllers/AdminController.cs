1b408687359733b4590182b074bcaa1e3528e4f2
Ivaylo <ivlincs@gmail.com>
2026-04-02 19:47:10 +0300

    Add Admin Area with AdminController and dashboard view

---
+++ ShoeTracker.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿namespace ShoeTracker.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    using ShoeTracker.Data;
+
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly ShoeTrackerDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(ShoeTrackerDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            int totalShoe = await _context.Shoes.CountAsync();
+            int totalRuns = await _context.Runs.CountAsync();
+            int totalUsers = await _userManager.Users.CountAsync();
+            int totalComments = await _context.Comments.CountAsync();
+
+            ViewBag.TotalShoe = totalShoe;
+            ViewBag.TotalRuns = totalRuns;
+            ViewBag.TotalUsers = totalUsers;
+            ViewBag.TotalComments = totalComments;
+
+            ViewBag.RecentShoes = await _context.Shoes
+                .Include(s => s.Category)
+                .OrderByDescending(s => s.PurchaseDate)
+                .Take(5)
+                .ToListAsync();
+
+            return View();
+
+        }
+    }
+}
--- ShoeTracker.Web/Program.cs
+++ ShoeTracker.Web/Program.cs
@@ -62,6 +62,9 @@ namespace ShoeTracker.Web
             app.UseAuthorization();
 
             app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
